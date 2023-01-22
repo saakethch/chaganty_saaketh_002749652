25f1d617d0e205746db2e3fa44e53c0b4092e3fe
Saaketh Ch <chaganty.s@northeastern.edu>
2023-01-21 19:42:06 -0500

    Assignment-0.done

--- src/main/java/Model/HelloWorld.java
+++ src/main/java/Model/HelloWorld.java
@@ -11,22 +11,21 @@ package Model;
  * @author 15512
  */
 public class HelloWorld {
+    public String name = "John";
+    public int age = 23;
+    public double salary = 65000.00;
+    public boolean isEmployed = true;
+    public char initial = 'J';
+    public int n = 10;
+    public int[] arr = new int[n];
+    
     public static void main(String[] args) {
         System.out.println("Hello World");
-        
-        String name = "John";
-        System.out.println("Name: " + name);
-        
-        int age = 23;
-        System.out.println("Age: " + age);
-
-        double salary = 65000.00;
-        System.out.println("Salary: " + salary);
-
-        boolean isEmployed = true;
-        System.out.println("Employed: " + isEmployed);
-
-        char initial = 'J';
-        System.out.println("Initial: " + initial);
+        HelloWorld hw = new HelloWorld(); 
+        System.out.println("Name: " + hw.name);
+        System.out.println("Age: " + hw.age);
+        System.out.println("Salary: " + hw.salary);
+        System.out.println("Employed: " + hw.isEmployed);
+        System.out.println("Initial: " + hw.initial);
     }
 }
