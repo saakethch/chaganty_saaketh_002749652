c45b9457a8a3480f1de7dbfc3a391ed82d576b1d
Saaketh Ch <chaganty.s@northeastern.edu>
2023-01-21 19:18:23 -0500

    Assignment-0.done

---
+++ src/main/java/Model/HelloWorld.java
@@ -0,0 +1,32 @@
+package Model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author 15512
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+        String name = "John";
+        System.out.println("Name: " + name);
+        
+        int age = 23;
+        System.out.println("Age: " + age);
+
+        double salary = 65000.00;
+        System.out.println("Salary: " + salary);
+
+        boolean isEmployed = true;
+        System.out.println("Employed: " + isEmployed);
+
+        char initial = 'J';
+        System.out.println("Initial: " + initial);
+    }
+}
