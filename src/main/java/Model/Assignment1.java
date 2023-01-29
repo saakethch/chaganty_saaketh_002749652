e0fa2d462bf155587923ce161c0b78e7a532051d
Saaketh Ch <chaganty.s@northeastern.edu>
2023-01-29 16:14:14 -0500

    UI Screeens Done

---
+++ src/main/java/Model/Assignment1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Model;
+
+/**
+ *
+ * @author 15512
+ */
+public class Assignment1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
